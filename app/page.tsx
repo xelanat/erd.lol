8826dafb4ff78135a2c877780ba0b6bbc4f8cb0c
Alex Tan <alex.hui.tan@gmail.com>
2024-03-09 19:49:25 -0800

    feat: write the beginnings of a landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
+import Head from 'next/head'
+import Link from 'next/link'
 
-const HomePage = () => {
-  
-}
+const LandingPage = () => (
+  <div>
+    <Head>
+      <title>MermaidJS Diagram Tool</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-export default HomePage
+    <main className="font-retro bg-retro-blue text-white">
+      <section className="text-center p-12">
+        <h1 className="text-3xl">MermaidJS Diagrams</h1>
+        <p>Create ERD diagrams with ease.</p>
+        {/* <button className="mt-4 bg-retro-pink p-2 rounded" onClick={() => }>Try It Now</button> */}
+        <Link className="mt-4 bg-retro-pink p-2 rounded text-white" href="./chart">
+          Try It Now
+        </Link>
+      </section>
+      <section className="p-12 bg-retro-green"></section>
+    </main>
+    {/* Footer */}
+    <footer className="text-center p-4 bg-retro-yellow">
+        © 2024 Alex Tan. All rights reserved.
+      </footer>
+  </div>
+)
+
+export default LandingPage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'retro-blue': '#0a2463',
+        'retro-pink': '#fb3640',
+        'retro-yellow': '#ffc857',
+        'retro-green': '#3f88c5',
+      },
+      fontFamily: {
+        'retro': ['Helvetica', "Arial"],
       },
     },
   },
