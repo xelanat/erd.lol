8975480ae3622c8b1b1d808f21822c91292cc250
Alex Tan <alex.hui.tan@gmail.com>
2024-03-10 10:40:06 -0700

    tweak: landing page verbiage for ER diagrams

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const LandingPage = () => (
     <main className="patterned-background h-screen">
       <section className="bg-white p-12 w-25 h-25">
         <h1 className="text-3xl text-black font-extrabold">erd.lol</h1>
-        <div className="my-4 text-black font-bold">Create ERD diagrams with ease 🤭</div>
+        <div className="my-4 text-black font-bold">Create entity-relationship diagrams with ease 🤭</div>
         <div className="mb-4 text-black font-bold">Built on MermaidJS.</div>
         <Link
           className="bg-black p-2 rounded text-white transition duration-200 border-2 hover:border-black hover:border-dotted hover:bg-white hover:text-black font-bold"
